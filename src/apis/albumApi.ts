767baf86ee48abad521c62076fe2746b7e565d01
hj522 <11_22nd@naver.com>
2025-06-06 06:08:53 +0900

    [commit] login test

--- src/apis/albumApi.ts
+++ src/apis/albumApi.ts
@@ -1,10 +1,10 @@
 import axios from 'axios';
-import { SPOTIFY_BASE_URL } from '../configs/commonConfig';
+import { REACT_APP_SPOTIFY_BASE_URL } from '../configs/commonConfig';
 import { GetNewReleasesResponse } from '../models/album';
 
 export const getNewReleases = async (clientCredentialToken: string): Promise<GetNewReleasesResponse> => {
     try {
-        const response = await axios.get(`${SPOTIFY_BASE_URL}/browse/new-releases?limit=6`, {
+        const response = await axios.get(`${REACT_APP_SPOTIFY_BASE_URL}/browse/new-releases?limit=6`, {
             headers: {
                 Authorization: `Bearer ${clientCredentialToken}`,
             },
